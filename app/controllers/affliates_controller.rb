6edbc6dc3679fc419637d8c30adb719b5d44daf7
MUN HOH <JACKLMH94@GMAIL.COM>
2018-01-18 11:25:26 +0800

    Add Affliates controller

---
+++ app/controllers/affliates_controller.rb
@@ -0,0 +1,2 @@
+class AffliatesController < ApplicationController
+end
---
+++ test/controllers/affliates_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AffliatesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
