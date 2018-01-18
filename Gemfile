b0fe814e23d09c51ab131abad9583449ba70190e
CCHINCHEE <CHONGCC90@GMAIL.COM>
2018-01-18 23:50:48 +0800

    added google login

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'city-state'
 gem 'figaro'
 # Facebook Login
 gem 'omniauth-facebook'
+# Google Login
+gem 'omniauth-google-oauth2'
 # Use clearance GEM
 gem 'clearance'
 # Use jquery as the JavaScript library
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,33 @@
   class SessionsController < Clearance::SessionsController
     def create_from_omniauth
       auth_hash = request.env["omniauth.auth"]
-      authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) ||  Authentication.create_with_omniauth(auth_hash)
+      authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
 
       # if: previously already logged in with OAuth
-      if authentication.user
+      if authentication != nil
         user = authentication.user
         authentication.update_token(auth_hash)
-        @next = "/"
-        @notice = "Signed in!"
+        @next = "/", @notice = "Signed in!"
       # else: user logs in with OAuth for the first time
+      elsif (user = User.find_by(email: auth_hash['info']['email'])) != nil
+        authentication = Authentication.create_with_omniauth(auth_hash)
+        authentication.update(user_id: user.id)
       else
+        authentication = Authentication.create_with_omniauth(auth_hash)
         user = User.create_with_auth_and_hash(authentication, auth_hash)
         # you are expected to have a path that leads to a page for editing user details
-        @next = "/"
+        #@next = edit_user_path(user)
         @notice = "User created. Please confirm or edit details"
       end
 
       sign_in(user)
-      redirect_to @next, :notice => @notice
+      redirect_to "/", :info => @notice
     end
 
     def url_after_destroy
-      "/"
-    end
+        "/"
+    end 
     
   end
+
+  
