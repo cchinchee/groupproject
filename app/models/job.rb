4c24a32c560f454c62d4d98f8b2033f2c2374db8
cheahhowong <cheahhowong@hotmail.com>
2018-01-20 10:17:52 +0800

    show the google map

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,11 +3,10 @@ class Job < ApplicationRecord
     belongs_to :affiliate
 
     def full_street_address
- 		[address,city,state].compact.join(',')
+ 		[address,state,city].compact.join(',')
 	end
 
-	def get_location
-    	geocoded_by :full_street_address
-		after_validation :geocode
-	end 
+	geocoded_by :full_street_address
+	after_validation :geocode
+	 
 end
