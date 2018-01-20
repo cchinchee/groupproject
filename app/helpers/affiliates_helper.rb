246cdfac43a1550b7815bdb94e6eb744bf701dad
MUN HOH <JACKLMH94@GMAIL.COM>
2018-01-20 10:55:35 +0800

    Repair seeds.rb for affiliate

--- app/helpers/affiliates_helper.rb
+++ app/helpers/affiliates_helper.rb
@@ -1,15 +1,2 @@
 module AffiliatesHelper
-
-	def affiliate_signed_in?
-		if Affiliate.all != nil
-			session[:affiliate_id] != nil
-		end
-	end
-
-	def current_affiliate
-
-		if session[:affiliate_id] != nil
-			@current_affiliate = Affiliate.find(session[:affiliate_id])
-		end
-	end
 end
