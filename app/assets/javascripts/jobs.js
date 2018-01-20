e8e7d9a30a7e975073f356239b3eb05083dc4d06
MUN HOH <JACKLMH94@GMAIL.COM>
2018-01-20 10:16:40 +0800

    Added calendar for job posting

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -3,6 +3,15 @@
 
 document.addEventListener("turbolinks:load", function(){
 
+	if (document.querySelector("#job_start_date")){
+		startDate = document.getElementById("job_start_date")
+		startTime = document.getElementById("job_start_time")
+
+		flatpickr(startDate, {altInput:true, minDate: new Date().fp_incr(1)})
+		flatpickr(startTime, {altInput: true, enableTime: true, noCalendar: true, dateFormat: "H:i", minDate: "09:00",
+    maxDate: "18:00"})
+	}
+
 	if (document.querySelector("#job_category")){
 		const category = document.querySelector("#job_category")
 		const priceInput = document.querySelector("#job_price")
